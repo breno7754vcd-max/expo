07f491aa55786a14845c13a6d9af9782db9c2ca5
Vojtech Novak <vonovak@gmail.com>
2025-12-17 16:49:09 +0000

    [notifications] deprecate NotificationImportance.UNSPECIFIED

--- packages/expo-notifications/build/setNotificationChannelAsync.android.js
+++ packages/expo-notifications/build/setNotificationChannelAsync.android.js
@@ -1,8 +1,14 @@
 import { UnavailabilityError } from 'expo-modules-core';
 import NotificationChannelManager from './NotificationChannelManager';
+import { AndroidImportance, } from './NotificationChannelManager.types';
 export default async function setNotificationChannelAsync(channelId, channel) {
     if (!NotificationChannelManager.setNotificationChannelAsync) {
         throw new UnavailabilityError('Notifications', 'setNotificationChannelAsync');
+    }
+    if (channel?.importance === AndroidImportance.UNSPECIFIED) {
+        console.warn(`Warning: You are setting the importance of the notification channel "${channelId}" to "UNSPECIFIED". ` +
+            `This may lead to errors on some Android versions. ` +
+            `Consider using AndroidImportance.DEFAULT instead.`);
     }
     return await NotificationChannelManager.setNotificationChannelAsync(channelId, channel);
 }
--- packages/expo-notifications/src/setNotificationChannelAsync.android.ts
+++ packages/expo-notifications/src/setNotificationChannelAsync.android.ts
@@ -1,7 +1,11 @@
 import { UnavailabilityError } from 'expo-modules-core';
 
 import NotificationChannelManager from './NotificationChannelManager';
-import { NotificationChannelInput, NotificationChannel } from './NotificationChannelManager.types';
+import {
+  NotificationChannelInput,
+  NotificationChannel,
+  AndroidImportance,
+} from './NotificationChannelManager.types';
 
 export default async function setNotificationChannelAsync(
   channelId: string,
@@ -11,5 +15,12 @@ export default async function setNotificationChannelAsync(
     throw new UnavailabilityError('Notifications', 'setNotificationChannelAsync');
   }
 
+  if (channel?.importance === AndroidImportance.UNSPECIFIED) {
+    console.warn(
+      `Warning: You are setting the importance of the notification channel "${channelId}" to "UNSPECIFIED". ` +
+        `This may lead to errors on some Android versions. ` +
+        `Consider using AndroidImportance.DEFAULT instead.`
+    );
+  }
   return await NotificationChannelManager.setNotificationChannelAsync(channelId, channel);
 }
